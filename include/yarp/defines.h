be6bb441ebcd799c3f03687745d532c7d921f1ce
Benoit Daloze <eregontp@gmail.com>
2023-04-09 12:59:27 +0200

    Define _XOPEN_SOURCE so strnlen() is declared by string.h
    * Also modern systems should all have strnlen(), so optimistically remove the fallback.

---
+++ include/yarp/defines.h
@@ -0,0 +1,10 @@
+// This files must be required before any system header
+// as it influences which functions system headers declare.
+
+#ifndef YARP_DEFINES_H
+#define YARP_DEFINES_H
+
+// For strnlen(), etc
+#define _XOPEN_SOURCE 700
+
+#endif
